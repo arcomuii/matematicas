7fdc25fb9ee7f7447eccad6dcbff70c4fb44b07e
Mauricio Salinas <mauricio.salinas@metodika.mx>
2026-05-22 17:30:28 -0600

    Agregar analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router"
+import { Analytics } from "@vercel/analytics/react"
 import Layout from "./components/Layout"
 
 import '@mantine/core/styles.css';
@@ -15,19 +16,22 @@ import {
 
 const App = () => {
 	return (
-		<MantineProvider>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Layout />}>
-						<Route index element={<Home />} />
-						<Route path="/disenos" element={<Disenos />} />
-						<Route path="/matematicas" element={<Matematicas />} />
-						<Route path="/tablas" element={<Tablas />} />
-						{/* <Route path="/contacto" element={<Contacto />} /> */}
-					</Route>
-				</Routes>
-			</BrowserRouter>
-		</MantineProvider>
+		<>
+			<MantineProvider>
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Layout />}>
+							<Route index element={<Home />} />
+							<Route path="/disenos" element={<Disenos />} />
+							<Route path="/matematicas" element={<Matematicas />} />
+							<Route path="/tablas" element={<Tablas />} />
+							{/* <Route path="/contacto" element={<Contacto />} /> */}
+						</Route>
+					</Routes>
+				</BrowserRouter>
+			</MantineProvider>
+			<Analytics />
+		</>
 	)
 }
 export default App
